217a72a85ec32acc425acea8f47287428d373ef6
John S <johnc.sime@gmail.com>
2021-12-09 14:07:26 -0500

    pulled day one solution from online

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import axios from "axios";
+import axios from 'axios';
 
-function CardGrabber() {
-  const [cardDets, setCardDets] = useState([])
-  useEffect(()=>{
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-    .then((resp)=>{
-      setCardDets(resp.data)
-    })
-    .catch((err)=>{
-      console.error(err)
-    })
-  }, [])
-  return (
-    <div className='imgCard'>
-      <h3 className='imgCardTitle'>{cardDets.title}</h3>
-      <img className='imgCardImg' src={cardDets.hdurl} alt='Cool APOD!'></img>
-      <p className='imgCardDes'>{cardDets.description}</p>
-      <p className='imgCardCopyright'>{cardDets.copyright}</p>
-    </div>
-  )
-}
-CardGrabber();
+import NasaCard from './Components/NasaCard';
 
 function App() {
+  const [nasaData, setNasaData] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY ')
+      .then(res => {
+        setNasaData(res.data);
+      }).catch(err => console.error(err));
+  }, [])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      { nasaData && <NasaCard data={nasaData} /> }
     </div>
   );
 }
